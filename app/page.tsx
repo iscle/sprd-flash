2b20210179bc52e95a7801932bd2fade4a361d78
Iscle <albertiscle9@gmail.com>
2024-10-29 22:33:06 +0100

    Add temporary page design

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,56 @@
 'use client';
 
-import { ChangeEvent, MouseEventHandler, useState } from "react";
-import SPRDDevice, { Packet } from "./lib/SPRDDevice";
+import { ChangeEvent, useState } from "react";
+import SPRDDevice from "./lib/SPRDDevice";
+import { Button, Input } from "@mui/material";
+import Box from '@mui/material/Box';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
+}));
 
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
@@ -72,11 +121,40 @@ export default function Home() {
   }
 
   return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleButtonClick}>
-        Run!
-      </button>
-    </div>
+    <SignInContainer direction="column" justifyContent="space-between">
+      <Card variant="outlined">
+        <Box
+          component="form"
+          noValidate
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+            gap: 2,
+          }}
+        >
+          <FormControl>
+            <FormLabel htmlFor="payload">Payload</FormLabel>
+            <Input
+              id="payload"
+              fullWidth
+              disableUnderline
+              required
+              color={'primary'}
+              type="file"
+              sx={{ ariaLabel: 'payload' }}
+              onChange={handleFileChange} />
+          </FormControl>
+          
+          <Button
+            fullWidth
+            variant="contained"
+            onClick={handleButtonClick}
+          >
+            Run!
+          </Button>
+        </Box>
+      </Card>
+    </SignInContainer>
   );
 }
