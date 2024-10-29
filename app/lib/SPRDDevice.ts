4be66bf1764572038a23bb38214ba79a1e7af008
Iscle <albertiscle9@gmail.com>
2024-10-29 02:06:02 +0100

    Fixed communication

--- app/lib/SPRDDevice.ts
+++ app/lib/SPRDDevice.ts
@@ -36,59 +36,17 @@ export default class SPRDDevice {
     private rcvBuf = new Uint8Array(SPRDDevice.HDLC_FRAME_MAX_SIZE)
     private rcvLen = 0
 
-	async open(): Promise<boolean> {
-        let device: USBDevice
-        
-        try {
-            device = await navigator.usb.requestDevice({ filters: [{
-                vendorId: 0x1782,
-                productId: 0x4d00
-            }]})
-        } catch (error) {
-            if (error instanceof DOMException && error.name === 'NotFoundError') {
-                console.error('No device selected')
-                return false
-            }
+	async open() {
+        const device = await navigator.usb.requestDevice({ filters: [{
+            vendorId: 0x1782,
+            productId: 0x4d00
+        }]})
 
-            console.error('Error requesting device:', error)
-            return false
-        }
-
-        if (!this.findConfigAndIface(device)) {
-            console.error('No suitable configuration/interface found')
-            return false
-        }
-
-        try {
-            await device.open()
-        } catch (error) {
-            console.error('Error opening device:', error)
-            return false
-        }
-
-        try {
-            await device.selectConfiguration(this.config)
-        } catch (error) {
-            console.error('Error selecting configuration:', error)
-            await device.close()
-            return false
-        }
-
-        try {
-            await device.claimInterface(this.iface)
-        } catch (error) {
-            console.error('Error claiming interface:', error)
-            await device.close()
-            return false
-        }
-
-        try {
-            await device.selectAlternateInterface(this.iface, this.alt)
-        } catch (error) {
-            console.error('Error selecting alternate interface:', error)
-            await device.close()
-            return false
-        }
+        await this.findConfigAndIface(device)
+        await device.open()
+        await device.selectConfiguration(this.config)
+        await device.claimInterface(this.iface)
+        await device.selectAlternateInterface(this.iface, this.alt)
 
         this.device = device
 
@@ -97,20 +55,19 @@ export default class SPRDDevice {
 
     async sendHello(): Promise<string> {
         const data = new Uint8Array([SPRDDevice.HDLC_FLAG])
-        await this.transferOut(data)
+        if (!await this.transferOut(data)) throw Error('Failed to transferOut')
         const response = await this.receivePacket()
         if (response.type !== HDLC_TYPE.REP_VER) throw Error()
         if (response.data === undefined) throw Error()
         return new TextDecoder('utf-8').decode(response.data.slice(0, response.data.byteLength - 1))
     }
 
-    async sendConnect(): Promise<boolean> {
-        if (!await this.sendPacket(new Packet(HDLC_TYPE.CMD_CONNECT))) throw Error()
+    async sendConnect() {
+        await this.sendPacket(new Packet(HDLC_TYPE.CMD_CONNECT))
         await this.receiveAck()
-        return true
     }
 
-    async sendStartData(address: number, length: number): Promise<boolean> {
+    async sendStartData(address: number, length: number) {
         const buffer = new ArrayBuffer(8)
         const view = new DataView(buffer)
         const array = new Uint8Array(buffer)
@@ -118,26 +75,48 @@ export default class SPRDDevice {
         view.setUint32(0, address)
         view.setUint32(4, length)
 
-        if (!await this.sendPacket(new Packet(HDLC_TYPE.CMD_START_DATA, array))) throw Error()
+        await this.sendPacket(new Packet(HDLC_TYPE.CMD_START_DATA, array))
         await this.receiveAck()
-        return true
     }
 
-    async sendMidstData(data: Uint8Array): Promise<boolean> {
-        if (!await this.sendPacket(new Packet(HDLC_TYPE.CMD_MIDST_DATA, data))) throw Error()
+    async sendMidstData(data: Uint8Array) {
+        await this.sendPacket(new Packet(HDLC_TYPE.CMD_MIDST_DATA, data))
         await this.receiveAck()
-        return true
     }
 
-    async sendEndData(): Promise<boolean> {
-        if (!await this.sendPacket(new Packet(HDLC_TYPE.CMD_END_DATA))) throw Error()
+    async sendEndData() {
+        await this.sendPacket(new Packet(HDLC_TYPE.CMD_END_DATA))
         await this.receiveAck()
-        return true
+    }
+
+    async sendPayload(address: number, data: Uint8Array) {
+        await this.sendStartData(address, data.byteLength)
+        const chunkSize = 528
+        for (let i = 0; i < data.byteLength; i += chunkSize) {
+            await this.sendMidstData(data.slice(i, i + chunkSize))
+        }
+        await this.sendEndData()
+    }
+
+    /*
+     * Jumps to the specified address by overwritting a specific lr value in the stack.
+     */
+    async sendJumpToPayload(stackLr: number, address: number, is64bit: boolean) {
+        await this.sendStartData(stackLr, 8)
+
+        const buffer = new ArrayBuffer(is64bit ? 8 : 4)
+        const view = new DataView(buffer)
+        const array = new Uint8Array(buffer)
+
+        view.setUint32(0, address, true)
+        if (is64bit) view.setUint32(4, 0, true)
+
+        await this.sendMidstData(array)
     }
 
     private async receiveAck() {
         const response = await this.receivePacket()
-        if (response.type !== HDLC_TYPE.REP_ACK) throw Error()
+        if (response.type !== HDLC_TYPE.REP_ACK) throw Error('Received unexpected response')
     }
 
     /*
@@ -170,7 +149,7 @@ export default class SPRDDevice {
                 throw Error('Failed to transferIn')
             }
 
-            console.debug('rcvBuf:', Array.prototype.map.call(
+            console.log('rcvBuf:', Array.prototype.map.call(
                 this.rcvBuf.slice(0, this.rcvLen),
                 x => ('00' + x.toString(16)).slice(-2)
             ).join(''))
@@ -215,17 +194,20 @@ export default class SPRDDevice {
 
         const type = view.getUint16(0)
         const dataLength = view.getUint16(2)
+        const data = dataLength > 0 ? new Uint8Array(buffer, 4, dataLength) : undefined
+        const crc = view.getUint16(4 + dataLength)
+        const expectedCrc = this.bromCrc(array, 0, 4 + dataLength)
+        if (crc !== expectedCrc) throw Error(`CRC mismatch: expected ${expectedCrc}, got ${crc}`)
 
         return new Packet(
             type,
-            new Uint8Array(buffer, 4, dataLength)
+            data
         )
     }
 
-    private async sendPacket(packet: Packet): Promise<boolean> {
+    private async sendPacket(packet: Packet) {
         const dataLength = packet.data ? packet.data.byteLength : 0
-        /* Double the length to account for escaped bytes */
-        const buffer = new ArrayBuffer(SPRDDevice.HDLC_FRAME_MIN_SIZE + dataLength * 2)
+        const buffer = new ArrayBuffer(SPRDDevice.HDLC_FRAME_MIN_SIZE * 2 + dataLength * 2)
         const view = new DataView(buffer)
         const array = new Uint8Array(buffer)
 
@@ -238,27 +220,26 @@ export default class SPRDDevice {
         view.setUint8(7 + dataLength, SPRDDevice.HDLC_FLAG)
         
         /* Escape HDCL_FLAG and HDLC_ESCAPE bytes */
-        let i = 0
-        let j = 0
-        
-        array[j++] = array[i++] /* HDLC_FLAG */
-        for (; i < SPRDDevice.HDLC_FRAME_MIN_SIZE - 2 + dataLength; i++) {
+        let i = SPRDDevice.HDLC_FRAME_MIN_SIZE + dataLength - 1
+        let j = array.byteLength - 1
+
+        array[j--] = array[i--] /* HDLC_FLAG */
+        for (; i > 0; i--) {
             const b = array[i]
             if (b === SPRDDevice.HDLC_FLAG || b === SPRDDevice.HDLC_ESCAPE) {
-                array[j++] = SPRDDevice.HDLC_ESCAPE
-                array[j++] = b ^ SPRDDevice.HDLC_ESCAPE_MASK
+                array[j--] = b ^ SPRDDevice.HDLC_ESCAPE_MASK
+                array[j--] = SPRDDevice.HDLC_ESCAPE
             } else {
-                array[j++] = b
+                array[j--] = b
             }
         }
-        array[j++] = array[i++] /* HDLC_FLAG */
+        array[j] = array[i] /* HDLC_FLAG */
 
-        await this.transferOut(buffer.slice(0, j))
-
-        return true
+        if (!await this.transferOut(buffer.slice(j, buffer.byteLength))) throw Error('Failed to transferOut')
     }
 
-    private bromCrc(array: Uint8Array, offset: number, length: number): number {
+
+    private bromCrc(data: Uint8Array, offset: number, length: number): number {
         const CRC_16_L_SEED: number = 0x80
         const CRC_16_L_POLYNOMIAL: number = 0x8000
         const CRC_16_POLYNOMIAL: number = 0x1021
@@ -270,11 +251,13 @@ export default class SPRDDevice {
                 if ((crc & CRC_16_L_POLYNOMIAL) !== 0) {
                     crc <<= 1
                     crc ^= CRC_16_POLYNOMIAL
+                    crc &= 0xFFFF
                 } else {
                     crc <<= 1
+                    crc &= 0xFFFF
                 }
 
-                if ((array[i] & j) !== 0) {
+                if ((data[i] & j) !== 0) {
                     crc ^= CRC_16_POLYNOMIAL
                 }
             }
@@ -298,6 +281,8 @@ export default class SPRDDevice {
             return false
         }
 
+        console.log('Received data:', result)
+
         if (result.status === 'ok') {
             if (result.data) {
                 const array = new Uint8Array(result.data.buffer, result.data.byteOffset, result.data.byteLength)
@@ -310,16 +295,21 @@ export default class SPRDDevice {
         }
     }
 
-    private async transferOut(data: BufferSource) {
+    private async transferOut(data: ArrayBuffer) {
         if (!this.device) {
             throw Error('Device not open')
         }
+
+        console.log('Sending data:', Array.prototype.map.call(
+            new Uint8Array(data),
+            x => ('00' + x.toString(16)).slice(-2)
+        ).join(''))
 
         const result = await this.device.transferOut(this.epOut, data)    
         return result.status === 'ok' && result.bytesWritten === data.byteLength
     }
 
-    private async findConfigAndIface(device: USBDevice): Promise<boolean> {
+    private async findConfigAndIface(device: USBDevice) {
         for (const config of device.configurations) {
             for (const iface of config.interfaces) {
                 for (const alt of iface.alternates) {
@@ -336,11 +326,11 @@ export default class SPRDDevice {
                         }
                         console.log('Configuration:', config)
                         console.log('Interface:', iface)
-                        return true
+                        return
                     }
                 }
             }
         }
-        return false
+        throw Error('No suitable configuration/interface found')
     }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,76 @@
 'use client';
 
+import { ChangeEvent, MouseEventHandler, useState } from "react";
 import SPRDDevice, { Packet } from "./lib/SPRDDevice";
 
 export default function Home() {
-  return (
-    <button onClick={
-      async () => {
-        const sprd = new SPRDDevice();
+  const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
+
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0]; // Safely access the first file
+    if (file) {
+      setSelectedFile(file);
+    }
+  }
+
+  const handleButtonClick = async () => {
+    if (!selectedFile) {
+      alert('Please select a file first')
+      return
+    }
+
+    const sprd = new SPRDDevice();
         
-        if (!await sprd.open()) {
-          console.error('Error opening device')
-          return
-        }
+    await sprd.open()
 
-        const helloResponse = await sprd.sendHello();
-        console.debug('Hello response:', helloResponse)
-        if (helloResponse !== 'SPRD3') {
-          console.error('Received unknown response from bootrom')
-          return
-        }
+    const helloResponse = await sprd.sendHello();
+    console.log('Hello response:', helloResponse)
+    if (helloResponse !== 'SPRD3') {
+      console.error('Received unknown response from bootrom')
+      return
+    }
 
-        console.log('Device ready!')
+    console.log('Device ready!')
 
+    const stackLr = 0x3f58
+    const loadAddr = 0x5500
 
+    const readFileAsUint8Array = (file: File): Promise<Uint8Array> => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          if (reader.result) {
+            const arrayBuffer = reader.result as ArrayBuffer;
+            const uint8Array = new Uint8Array(arrayBuffer);
+            resolve(uint8Array);
+          } else {
+            reject(new Error("File reading failed"));
+          }
+        };
+        reader.onerror = () => {
+          reject(new Error("File reading error"));
+        };
+        reader.readAsArrayBuffer(file);
+      });
+    };
 
-      }
-    }>
-      Run!
-    </button>
+    const fileData = await readFileAsUint8Array(selectedFile);
+
+    console.log(`Sending payload to ${loadAddr.toString(16)}... (${fileData.length} bytes)`);
+
+    await sprd.sendPayload(0x5500, fileData);
+    console.log('Payload sent!');
+    await sprd.sendJumpToPayload(loadAddr + 0x200, stackLr, true);
+
+    console.log('fdl1 hello:', await sprd.sendHello());
+  }
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleButtonClick}>
+        Run!
+      </button>
+    </div>
   );
 }
