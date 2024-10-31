dcb007e3e3c5edc7d866494ea8e853713263ed1b
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-10-31 01:38:44 -0400

    feat: add sample guided download component

--- app/(tools)/unlock/page.tsx
+++ app/(tools)/unlock/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+import GuidedDownload from "@/component/GuidedDownload";
+
+function timeout(delay: number) {
+  return new Promise(res => setTimeout(res, delay));
+}
+
 export default function SampleRoute() {
-  return <div>Sample Route, to be populated later.</div>
+  return (
+    <div className="p-4">
+      This is a sample route, which will always pretend to succeed.
+
+      <GuidedDownload onWaitForDevice={async () => {
+        await timeout(5000);
+        return true;
+      }} />
+    </div>
+  )
 }
