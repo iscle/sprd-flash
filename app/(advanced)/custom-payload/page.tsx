263ef1f5870e694c9d25d6395e79a94aa04e4fd9
Greg Willard (r3pwn) <wickett06@gmail.com>
2024-11-06 02:12:17 -0500

    feat(home): populate with cards

--- app/(advanced)/custom-payload/page.tsx
+++ app/(advanced)/custom-payload/page.tsx
@@ -7,11 +7,11 @@ import { Button, Input } from "@mui/material";
 import Box from '@mui/material/Box';
 import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
-import Stack from '@mui/material/Stack';
 import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
 import { readFile } from "@/lib/util";
 import Socrates from "@/lib/Socrates";
+import { StyledBaseScreen } from "@/component/BaseScreen";
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
@@ -30,29 +30,6 @@ const Card = styled(MuiCard)(({ theme }) => ({
     boxShadow:
       'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
   }),
-}));
-
-const SignInContainer = styled(Stack)(({ theme }) => ({
-  height: 'calc(100dvh - var(--appbar-height, 64px))',
-  minHeight: '100%',
-  padding: theme.spacing(2),
-  [theme.breakpoints.up('sm')]: {
-    padding: theme.spacing(4),
-  },
-  '&::before': {
-    content: '""',
-    display: 'block',
-    position: 'absolute',
-    zIndex: -1,
-    inset: 0,
-    backgroundImage:
-      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
-    backgroundRepeat: 'no-repeat',
-    ...theme.applyStyles('dark', {
-      backgroundImage:
-        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
-    }),
-  },
 }));
 
 export default function CustomPayload() {
@@ -106,14 +83,10 @@ export default function CustomPayload() {
     await bootRom.sendPayload(loadAddr, fileData);
     console.log('Jumping to payload...');
     await bootRom.sendJumpToPayload(loadAddr + 0x200);
-
-    
-
-
   }
 
   return (
-    <SignInContainer direction="column" justifyContent="space-between">
+    <StyledBaseScreen direction="column" justifyContent="space-between">
       <Card variant="outlined">
         <Box
           component="form"
@@ -148,6 +121,6 @@ export default function CustomPayload() {
           </Button>
         </Box>
       </Card>
-    </SignInContainer>
+    </StyledBaseScreen>
   );
 }
---
+++ component/HomeCard.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import Link from 'next/link';
+import theme from '@/theme';
+
+type Props = {
+  text: string;
+  href: string;
+  description?: string;
+  icon?: typeof ArrowForwardIcon;
+}
+
+export default function HomeCard({ text, href, description, icon: Icon }: Props) {
+  return (
+    <Link href={href} aria-label={text}>
+      <Box sx={{
+        minWidth: 200,
+        transition: 'scale 150ms ease-in-out',
+        '&:hover': {
+          scale: 1.03
+        }
+      }}>
+        <Card variant="outlined">
+          <CardContent>
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: 1, alignItems: 'center', mb: 2 }}>
+              {!!Icon ? <Icon /> : undefined}
+              <Typography variant="h5" component="div">
+                {text}
+              </Typography>
+            </Box>
+            <Typography variant="body1">
+              {description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <ArrowForwardIcon sx={{ justifyContent: 'end', margin: 1, marginLeft: 'auto' }} />
+          </CardActions>
+        </Card>
+      </Box>
+    </Link>
+  );
+}
